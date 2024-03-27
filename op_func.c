2cde6b8ce2072e10ca4cfad342ba24775fdfde50
mornark <chrixiants@gmail.com>
2024-03-28 00:14:00 +0300

    edge case handling for pall, extern used for temp, bettyfied.

--- op_func.c
+++ op_func.c
@@ -1,20 +1,42 @@
 #include "monty.h"
+struct_t temp = NULL;
 
 /**
  * push - pushes an element to the stack
+ * @element: pointer to element to be added to stack
+ * @line_number: line number of command from buffer
  **/
 
 void push(struct_t **element, int line_number)
 {
-
-	if (ptr == NULL)
-	{
-		fprintf("Error: malloc failed")
-		exit(EXIT_FAILURE);
-	}
 	(*element)->next = temp;
 	(*element)->prev = NULL;
 	if (temp != NULL)
-		(*element)->prev = ptr;
+		(*element)->prev = *element;
 	temp = *element;
+}
+
+/**
+ * pall - displays all the elements in the stack
+ * @element: pointer to iterate the stack
+ * @line_number: line number of command from buffer
+ **/
+
+void pall(struct_t **element, int line_number)
+{
+	int i = 0;
+
+	*element = temp;
+	if (*element == NULL)
+	{
+		fprint(stderr, "yo");
+	}
+	else
+	{
+		while (element != NULL)
+		{
+			printf("%d\n", *(element)->n);
+			*element = *(element)->next;
+		}
+	}
 }
--- opcodes.c
+++ opcodes.c
@@ -2,30 +2,35 @@
 
 void opcodes(char **command, int line_number)
 {
-	int i = 0;
+	int i = 0, count = 0;
 	struct_t *element;
 	instruction_t arr[] = {{"push", push}, {"pall", pall}};
 
+	while (command[i] != NULL)
+		count++;
 	element = malloc(sizeof(struct_t));
-	if (element = NULL)
+	if (element == NULL)
 	{
-		fprinf(stderr, "Error: malloc failed");
+		fprintf(stderr, "Error: malloc failed");
 		exit(EXIT_FAILURE);
 	}
-
-	for (i = 0; command[1][i] != '\0'; i++)
+	if (count > 1)
 	{
-		if !(command[1][i] >= '0' && command[i] <= '9')
+		for (i = 0; command[1][i] != '\0'; i++)
 		{
-			fprintf(stderr, "invalid command");
-			exit(EXIT_FALURE);
+			if (!(command[1][i] >= '0' && command[i] <= '9'))
+			{
+				fprintf(stderr, "invalid command");
+				exit(EXIT_FALURE);
+			}
 		}
 	}
-	element->n = atoi(command[1]);
-	for (i = 0; arr[i] < 2; i++)
+	for (i = 0; i < 2; i++)
 	{
 		if (strcmp(command[0], arr[i]->opcode) == 0)
 		{
+			if (i == 0)
+				element->n = atoi(command[1]);
 			arr[i]->(*f)(&element, line_number);
 		}
 	}
